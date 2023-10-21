eb7b01c1f24b6d86b68961f7d45e5aa096acdcb9
DeminDenis1 <maxwelldenis888@gmail.com>
2023-10-21 19:38:21 +0500

    Task1

---
+++ Tyuiu.DeminDA.Sprint3.Task1.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DeminDA.Sprint3.Task1.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DeminDA.Sprint3.Task1.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ffd1effa-311f-4cbf-861d-9181d59f9059")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
