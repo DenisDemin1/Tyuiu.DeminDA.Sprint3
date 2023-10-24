34d9a3c20caa98e69b3331ddf7b09cdb070f56cd
DeminDenis1 <maxwelldenis888@gmail.com>
2023-10-24 18:52:10 +0500

    Task4

---
+++ Tyuiu.DeminDA.Sprint3.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DeminDA.Sprint3.Task4.V19.Lib;
+
+namespace Tyuiu.DeminDA.Sprint3.Task4.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculate()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 9.432;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DeminDA.Sprint3.Task4.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DeminDA.Sprint3.Task4.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DeminDA.Sprint3.Task4.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea087f2a-5b5d-4401-bdc1-c4842258e2fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
